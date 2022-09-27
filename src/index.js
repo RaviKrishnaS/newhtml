7bda00eb1f40b07fb59a37dc2ed3ce4b4b9aa595
ravi <itcdigpy05@iiht.tech>
2022-09-27 17:21:46 +0530

    first file saved

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const udemyapp = ()=>{
+    return(
+            <div className='ui comment'>
+                <div >
+                <h1>Hai this is Paragraph</h1>
+                    hai
+                </div>
+            </div>
+                
+        
+    )
+
+}
+ReactDom.render(
+<udemyapp/> ,
+document.querySelector('#root'))
